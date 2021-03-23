5abea3f55d66151eed2686e84cf24c27a0bc7442
M-Sieger <msieger1994@gmail.com>
2021-03-23 15:19:54 +0100

    ♻️ Refactor components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import ButtonLink from "../components/button";
 
 export default function Home() {
   return (
@@ -10,11 +9,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <ButtonLink label="your label" handleClick={""} />
-
-        <ButtonLink />
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
