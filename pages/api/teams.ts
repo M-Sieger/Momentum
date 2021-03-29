13f3269233ef08182697ed7283d2e8bb5e7a5ef0
M-Sieger <77615500+M-Sieger@users.noreply.github.com>
2021-03-29 09:31:38 +0200

    ➕ Add GET request for leagues and teams (#14)
    -  add samples.http
    - add team.ts and league.ts

---
+++ pages/api/teams.ts
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const response = await fetch(
+    "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=German%20Bundesliga"
+  );
+  const teams = await response.json();
+  res.status(200).json(teams);
+};
