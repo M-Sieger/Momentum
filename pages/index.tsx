5961d5f8776c528daf6811b321892d8fc373911d
M-Sieger <77615500+M-Sieger@users.noreply.github.com>
2021-03-30 18:30:16 +0200

    ➕ Add SearchInput for API (#15)
    - Add SearchInput in index.tsx
    - Rename GET requests

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [search, setSearch] = useState<string>("");
   return (
     <div className={styles.container}>
       <Head>
@@ -9,7 +11,18 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <label>
+          Search{" "}
+          <input
+            type="text"
+            placeholder="Search for Teams"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </label>
+        {search}
+      </main>
     </div>
   );
 }
