82923fb83e36daa14b3967919e766c4e8e38796f
M-Sieger <77615500+M-Sieger@users.noreply.github.com>
2021-03-31 17:25:48 +0200

    ➕  Add teamList component (#16)
    * ➕  Add teamList component
    
    - install SWR  https://www.npmjs.com/package/swr
    
    - add teamList component
    - add TeamTypes
    
    * ➕ Add teamList components
    
    - install SWR
    - Add teamlist components
    - Add TeamTypes

---
+++ pages/api/teamList.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import React from "react";
+import useSWR from "swr";
+import TeamList from "../../components/TeamList/teamList";
+import type { Team } from "../../utils/TeamTypes";
+import styles from "../../styles/Home.module.css";
+
+export type TeamListPageProps = { teams: Team[] };
+
+export const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function TeamListPage() {
+  const { data, error } = useSWR("/api/teams", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div className={styles.container}>loading</div>;
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Momentum</title>
+      </Head>
+      <main>
+        <TeamList teams={data} />
+      </main>
+    </div>
+  );
+}
