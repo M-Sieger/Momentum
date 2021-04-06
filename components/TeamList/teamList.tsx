87ca005bf43273b8452f9dc02c6277ad5237b9bb
M-Sieger <msieger1994@gmail.com>
2021-04-06 16:02:43 +0200

    💄 Add some CSS for teamList

--- components/TeamList/teamList.tsx
+++ components/TeamList/teamList.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import type { Team } from "../../utils/TeamTypes";
 import styles from "../TeamList/teamList.module.css";
 import Link from "next/link";
+import Header from "../Header/Header";
 
 export type TeamListProps = {
   teams: Team[];
@@ -9,12 +11,18 @@ export type TeamListProps = {
 const TeamList = ({ teams }: TeamListProps) => {
   return (
     <>
+      <span className={styles.ListHeader}>
+        <Header />
+      </span>
       <div>
         {teams.map((team) => (
-          <Link href={`/teams/${team.strTeam}`} key={team.strTeam}>
-            <div className={styles.listContainer} key={team.id}>
-              <img src={team.strTeamBadge} alt={team.strTeam} />
-              <p className={styles.teamName}>{team.strTeam}</p>
+          <Link href={`/teamDetails/${team.strTeam}`} key={team.strTeam}>
+            <div className={styles.listContainer} key={team.strTeam}>
+              <img
+                className={styles.teamListBadge}
+                src={team.strTeamBadge}
+                alt={team.strTeam}
+              />
             </div>
           </Link>
         ))}
